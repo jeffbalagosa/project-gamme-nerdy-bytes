d31d70dda4e61f2d8ed25f1676da02f7f77c4573
Jeff Balagosa <jeff.balagosa@gmail.com>
2023-12-15 15:04:39 -0800

    Refactor ChatFlyover component to conditionally render based on route

--- ghi/src/components/shared/ChatFlyover/ChatFlyover.js
+++ ghi/src/components/shared/ChatFlyover/ChatFlyover.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import Chat from "../../../pages/Chat/ChatPage";
 
 export default function ChatFlyover() {
-    const [isOpen, setIsOpen] = useState(false);
-    const handleChange = () => setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+  const handleChange = () => setIsOpen(!isOpen);
 
-    return (
-        <div id="chat-flyover" style={{ zIndex: 3, position: "relative" }}>
-            <button onClick={handleChange} id="buttons">
-                {isOpen ? "Close" : "Open"} Chat
-            </button>
-            <dialog open={isOpen}>
-                <Chat></Chat>
-            </dialog>
-        </div>
-    );
+  // Do not render on '/' or '/signup' routes
+  if (location.pathname === "/" || location.pathname === "/signup") {
+    return null;
+  }
+
+  return (
+    <div id="chat-flyover" style={{ zIndex: 3, position: "relative" }}>
+      <button onClick={handleChange} id="buttons">
+        {isOpen ? "Close" : "Open"} Chat
+      </button>
+      <dialog open={isOpen}>
+        <Chat></Chat>
+      </dialog>
+    </div>
+  );
 }
